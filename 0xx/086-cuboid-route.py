ea97eda52598a6882ae1bccbfdc2354220e5840c
fraserweist <fraserweist@gmail.com>
2017-06-01 19:38:06 -0400

    started 086

---
+++ 0xx/086-cuboid-route.py
@@ -0,0 +1,29 @@
+from math import *
+from tqdm import *
+
+def integral(xy,z):
+	c2 = (xy)**2 + z**2
+	sol = int(sqrt(c2))
+	return sol**2 == c2
+
+def main():
+	m = 30
+	target = 2000
+	solution = 0
+	lst = []
+	ctr = 0
+	while True:
+		for xy in range(3,2*m+1):
+			if integral(xy,m):
+				if xy <= m:
+					ctr += xy / 2
+				else:
+					ctr += m - (xy+1)/2 + 1
+		print m, ctr
+		if ctr > target:
+			solution = m
+			break
+		m += 1
+	print solution
+
+main()
