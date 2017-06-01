f619986c1de6f3eb3ea1139b7967ee0940cd8921
fraserweist <fraserweist@gmail.com>
2017-06-01 18:39:30 -0400

    solved 098!

--- 0xx/098-anagramic-squares.py
+++ 0xx/098-anagramic-squares.py
@@ -2,6 +2,17 @@
 from tqdm import *
 from math import *
 
+def str_map(s1, s2, n1):
+	n = len(s1)
+	nstr = str(n1)
+	mapping = dict()
+	for i in range(n):
+		mapping[s1[i]] = nstr[i]
+	n2 = ""
+	for i in range(n):
+		n2 += mapping[s2[i]]
+	return int(n2)
+
 def aligns(n, s):
 	if len(str(n)) != len(s):
 		return False
@@ -45,12 +56,35 @@ def main():
 			if len(words[j]) > 4:
 				if is_anagram(words[i], words[j]):
 					pairs.append((words[i],words[j]))
-	print pairs
-	start = 10000
+	pairs.sort(key=lambda (s1,s2): -len(s1))
+
+	start = 10000000000
 	ctr = int(sqrt(start))
-
+	solution, strings = 0, ("","")
+	
 	while True:
-		n = ctr**2
-
+		n1 = ctr**2
+		if n1 < 10000: break
+		lst = filter(lambda (s1,s2): len(s1) == len(str(n1)), pairs)
+		for (s1,s2) in lst:
+			if aligns(n1, s1):
+				n2 = str_map(s1,s2,n1)
+				if len(str(n2)) != len(str(n1)): break
+				if is_square(n2):
+					if n1 > solution:
+						solution, strings = n1, (s1, s2)
+					if n2 > solution:
+						solution, strings = n2, (s2, s1)
+			if aligns(n1, s2):
+				n2 = str_map(s2,s1,n1)
+				if len(str(n2)) != len(str(n1)): break
+				if is_square(n2):
+					if n1 > solution:
+						solution, strings = n1, (s1, s2)
+					if n2 > solution:
+						solution, strings = n2, (s2, s1)
+		ctr -= 1
+	s1,s2 = strings
+	print solution, str_map(s1,s2,solution), strings
 
 main()
